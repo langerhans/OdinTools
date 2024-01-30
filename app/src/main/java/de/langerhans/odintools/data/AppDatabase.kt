747ef2a1e23df300ab4ca3270d3a4dc98c7a7c8e
Max K <langerhans@users.noreply.github.com>
2024-01-30 20:51:37 +0100

    DB migration for perf and fan modes

--- app/src/main/java/de/langerhans/odintools/data/AppDatabase.kt
+++ app/src/main/java/de/langerhans/odintools/data/AppDatabase.kt
@@ -1,11 +1,16 @@
 package de.langerhans.odintools.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
 @Database(
     entities = [AppOverrideEntity::class],
-    version = 1
+    version = 2,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ],
+    exportSchema = true
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun appOverrideDao(): AppOverrideDao
