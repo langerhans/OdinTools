8ad2b8ebe6bc757daa7fa87cad3bcd298353e473
Max K <langerhans@users.noreply.github.com>
2024-05-25 20:38:50 +0200

    Fix release build being debuggable

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,6 +27,7 @@ android {
     buildTypes {
         release {
             initWith(buildTypes.getByName("debug"))
+            isDebuggable = false
             isMinifyEnabled = true
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
             applicationVariants.all {
